c65782619f1fc7b39141888249c007596d2b1fa0
wadizzo79 <caywadawi7@gmail.com>
2026-01-22 20:21:48 +0300

    New updates

--- calculator/script.js
+++ calculator/script.js
@@ -1,8 +1,8 @@
 
 
 function addition() {
-    let x = document.getElementById("fvalue").value;
-    let y = document.getElementById("svalue").value;
+    let x = document.getElementById("fvalue").value; // Assigns the value found in the input field with the id fvalue into x
+    let y = document.getElementById("svalue").value; // Assigns the value found in the input field with the id fvalue into y
     alert(Number(x) + Number(y));
 }
 
@@ -25,8 +25,8 @@ function division(){
 }
 
 function clearFields(){
-    const fields = document.querySelectorAll('.clearable-field');
-    fields.forEach(field => {
-        field.value = '';
+    const fields = document.querySelectorAll('.clearable-field'); // Looks through the entire html and returns every element with the class clearable-field
+    fields.forEach(field => { // Goes through the collection one item at a time and for each element found it temporary calls them field
+        field.value = ''; // Accesses the elements value and replaces whatever text/data was inside with an empty string
     });
 }
--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,9 +1,18 @@
+let toDo = [];
+
 function displayText(){
-    var inputValue = document.getElementById("task").value;
+    var inputValue = document.getElementById("task").value; // Gets the value from the input field and stores it in inputValue
+    
+    toDo.push(inputValue); // The value in inputValue is then stored in the array
 
-    let toDo = [];
+    document.getElementById("outputText").innerHTML = toDo.join("<br>"); // Displays the elements in the array onto the browser. .join("<br>") breaks the array into a new line
 
-    toDo.push(inputValue);
+    document.getElementById("task").value=""; // Clears the input field
+}
 
-    document.getElementById("outputText").innerHTML = toDo;
+function deleteTask(){
+    toDo.pop(); // Deletes the last field from the array
+
+    document.getElementById("outputText").innerHTML = toDo.join("<br>");
+
 }
