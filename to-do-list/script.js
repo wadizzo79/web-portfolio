af77bbdbd96b35aca98fe1ff93a8e74489e85c47
wadizzo79 <caywadawi7@gmail.com>
2026-01-24 17:39:16 +0300

    More updates

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,11 +1,18 @@
 let toDo = [];
 
-function displayText(){
-    var inputValue = document.getElementById("task").value; // Gets the value from the input field and stores it in inputValue
-    
-    toDo.push(inputValue); // The value in inputValue is then stored in the array
+let test = new Object();
 
-    document.getElementById("outputText").innerHTML = toDo.join("<br>"); // Displays the elements in the array onto the browser. .join("<br>") breaks the array into a new line
+function addTask(){
+    var inputValue = document.getElementById("task").value; // Gets the value from the input field and stores it in inputValue
+   
+    toDo.push({
+        task: inputValue, 
+        description: document.getElementById("task-description").value,
+}   ); // The value in inputValue is then stored in the array
+
+    document.getElementById("outputText").innerHTML = toDo; // Displays the elements in the array onto the browser. .join("<br>") breaks the array into a new line
+
+    //document.getElementById("outputText2").innerHTML = test.description;
 
     document.getElementById("task").value=""; // Clears the input field
 }
