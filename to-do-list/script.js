623fcde969562f5e27a8e42062111a0d1406579d
wadizzo79 <caywadawi7@gmail.com>
2026-01-27 23:04:20 +0300

    More changes

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,6 +1,5 @@
-let toDo = [];
+let toDo = []; // initializes the array
 
-let test = new Object();
 
 function addTask(){
     var inputValue = document.getElementById("task").value; // Gets the value from the input field and stores it in inputValue
@@ -8,18 +7,23 @@ function addTask(){
     toDo.push({
         task: inputValue, 
         description: document.getElementById("task-description").value,
+        priority: document.getElementById("task-priority").value, // An object storing the values from the input fields
 }   ); // The value in inputValue is then stored in the array
 
-    document.getElementById("outputText").innerHTML = toDo; // Displays the elements in the array onto the browser. .join("<br>") breaks the array into a new line
+    document.getElementById("outputText").innerHTML = toDo
+    .map(item => `${item.task}<br>${item.description}<br>${item.priority}`).join("<hr>"); // Loops thrrough the array one array at a time and returns a new array 
 
-    //document.getElementById("outputText2").innerHTML = test.description;
+    
 
-    document.getElementById("task").value=""; // Clears the input field
+    document.getElementById("task").value=""; 
+    document.getElementById("task-description").value=""; 
+    document.getElementById("task-priority").value=""; // Clears the input field
 }
 
 function deleteTask(){
     toDo.pop(); // Deletes the last field from the array
 
-    document.getElementById("outputText").innerHTML = toDo.join("<br>");
+    document.getElementById("outputText").innerHTML = toDo
+    .map(item => `${item.task}<br>${item.description}<br>${item.priority}`).join("<hr>");
 
 }
