04fa3decc8a9f6398ae2e19510b8bf5abb5e4689
wadizzo79 <caywadawi7@gmail.com>
2026-02-18 19:10:48 +0300

    update

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,3 +1,11 @@
-function generateWeather(){
-    
+async function generateWeather(){
+    try{
+    const weather = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m");
+    const data = await weather.json();
+
+    const interfaceDiv = document.getElementById("interface");
+    interfaceDiv.innerHTML = `<pre>${JSON.stringify(data.hourly.temperature_2m.slice(0,5), null, 2)}</pre>`
+    } catch(error) {
+        console.error("Error fetching weather:", error);
+    }
 }
