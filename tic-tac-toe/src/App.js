a3ee87d8aca6e617277f6a161a5027409912e8fa
wadizzo79 <caywadawi7@gmail.com>
2026-04-06 14:02:47 +0300

    update

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,8 +1,25 @@
-export default function Square() {
+function Square(){
+    return <button className="square">1</button>;
+}
+
+export default function Board() {
   return (
     <>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   );
 }
