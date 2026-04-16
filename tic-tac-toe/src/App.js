e491d053aa0f77a9eb7f3fce97aec22e5fc1eb11
wadizzo79 <caywadawi7@gmail.com>
2026-04-16 18:37:46 +0300

    update 16/4

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,10 +5,14 @@ function Square({value, onSquareClick}){
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     const nextSquares = squares.slice();
+    if (xIsNext){
+      
+    }
     nextSquares[i] = "X";
     setSquares(nextSquares);
   }
