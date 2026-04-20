0dcf2a72775e179607dba065840f2cda256eb16c
wadizzo79 <caywadawi7@gmail.com>
2026-04-20 19:07:43 +0300

    update 20/4

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -9,12 +9,17 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext){
-      
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
     }
-    nextSquares[i] = "X";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
