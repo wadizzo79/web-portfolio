1b8542f59f3f735af4e8b9653ef2ae962c013cd9
wadizzo79 <caywadawi7@gmail.com>
2026-05-06 20:19:54 +0300

    Create App.js

---
+++ to-do-list-react/src/App.js
@@ -0,0 +1,13 @@
+export default function TextArea() { // Text field to input new items
+  return (
+    <form method="post">
+    <>
+    <label>
+      Next item: <input name="inputValue" />
+    </label>
+    <button type="submit">Submit</button>
+    </>
+    </form>
+  )
+}
+
