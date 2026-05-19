60a257b9595bb03f4b121b0a8f3092a3cc84b1b9
wadizzo79 <caywadawi7@gmail.com>
2026-05-19 15:34:01 +0300

    Update App.js

--- to-do-list-react/src/App.js
+++ to-do-list-react/src/App.js
@@ -1,13 +1,55 @@
-export default function TextArea() { // Text field to input new items
+import React, { useState } from 'react';
+//import { useRef } from 'react';
+
+export default function App() {
+  const [items,setItems] = useState([]);
+
+  function addTodo(inputValue) {
+    setItems([...items, inputValue]);
+  }
+
+  return (
+    <>
+      <TextArea
+        onAddText={addTodo}
+      />
+      <TaskList
+        todos={items}
+      />
+    </>
+  )
+};
+
+function TextArea({ onAddText }) { // Text field to input new items
+  const [inputValue, setInputValue] = useState("");
+  //const inputRef = useRef(null);
+
   return (
     <form method="post">
     <>
     <label>
-      Next item: <input name="inputValue" />
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
     </label>
-    <button type="submit">Submit</button>
+    <button type="submit" onClick={() => {
+      setInputValue('');
+      onAddText(inputValue);
+    }}>Submit</button>
     </>
     </form>
   )
 }
+
+function TaskList({ todos }){
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li>
+          {todo} 
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+
 
